12eeb9f77fab4dac00a58da9a399c1f0ca446176
tavogus007 <gustavog095065@gmail.com>
2025-12-07 22:08:42 -0400

    Feat: :fire: Working with Reactive forms

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss'],
+})
+export class BasicFormComponent implements OnInit {
+  nameField = new FormControl(' Soy un control');
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.nameField.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  getNameValue() {
+    console.log(this.nameField.value);
+  }
+}
