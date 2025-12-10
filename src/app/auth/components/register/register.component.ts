01a8ca88d040aa76dc2f09acc39556dd3092f85c
tavogus007 <gustavog095065@gmail.com>
2025-12-10 19:00:10 -0400

    Feat: :art: Adding a new grupal validation methodCLASE 19

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,14 +3,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './../../../core/services/auth.service';
-import { MyValidators } from '../../../utils/validators'
+import { MyValidators } from '../../../utils/validators';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
@@ -21,25 +20,28 @@ export class RegisterComponent implements OnInit {
     this.buildForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   register(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const value = this.form.value;
-      this.authService.createUser(value.email, value.password)
-      .then(() => {
+      this.authService.createUser(value.email, value.password).then(() => {
         this.router.navigate(['/auth/login']);
       });
     }
   }
 
   private buildForm() {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required, MyValidators.validatePassword]],
-    });
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        password: ['', [Validators.required, MyValidators.validatePassword]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: MyValidators.matchPassword,
+      }
+    );
   }
-
 }
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -18,6 +18,18 @@ export class MyValidators {
     }
     return null;
   }
+
+  static matchPassword(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    if (password == confirmPassword) {
+      return null;
+    } else {
+      return {
+        match_password: true,
+      };
+    }
+  }
 }
 
 function containsNumber(value: string) {
