66b896d08cb874b48bad3e7aee242f6c4e8970dd
tavogus007 <gustavog095065@gmail.com>
2025-12-10 20:12:54 -0400

    Feat: :art: Categories Management

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm
+  }
 
   ngOnInit(): void {
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      iamge: ['', Validators.required],
+    })
+  }
+
+  get nameField(){
+    return this.form.get('name');
+  }
+
+  get iamgeField(){
+    return this.form.get('iamge');
   }
 
 }
