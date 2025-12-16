3f501ca45adea4bb6147165b39f7f5ffb1628a99
tavogus007 <gustavog095065@gmail.com>
2025-12-16 17:20:18 -0400

    Feat: :art: Completing the category component

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { CategoryService } from './../../../../core/services/categories-service';
+
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -10,27 +14,45 @@ export class CategoryFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoryService,
+    private router: Router
   ) {
-    this.buildForm
+    this.buildForm();
   }
 
   ngOnInit(): void {
   }
 
-  private buildForm(){
+  private buildForm() {
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
-      iamge: ['', Validators.required],
-    })
+      image: ['', Validators.required]
+    });
   }
 
-  get nameField(){
+  get nameField() {
     return this.form.get('name');
   }
 
-  get iamgeField(){
-    return this.form.get('iamge');
+  get imageField() {
+    return this.form.get('image');
+  }
+
+  save() {
+    if (this.form.valid) {
+      this.createCategory();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private createCategory() {
+    const data = this.form.value;
+    this.categoriesService.createCategory(data)
+    .subscribe(rta => {
+      this.router.navigate(['/admin/categories']);
+    });
   }
 
 }
---
+++ src/app/core/services/categories-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { environment } from './../../../environments/environment';
+import { Category } from './../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+    constructor(
+        private http: HttpClient
+    ) {}
+
+    getAllCategories(){
+        return this.http.get<Category[]>(`${environment.url_api}/categories/`);
+    }
+
+    createCategory(data: Partial<Category>){
+        return this.http.post<Category>(`${environment.url_api}/categories/`, data);
+    }
+
+    updatecategory(id: string,data: Partial<Category>){
+        return this.http.put<Category>(`${environment.url_api}/categories/${id}`, data);
+    }
+ 
+}
